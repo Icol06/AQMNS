4b36550c39a649a55465194ad340c75fda994c15
faisholl <faisho.el.ha@gmail.com>
2017-03-25 17:54:37 +0700

    INITIAL_COMMIT

--- AQMNS/NavAst/app/src/main/java/com/example/icol/navast/MapsActivity.java
+++ AQMNS/NavAst/app/src/main/java/com/example/icol/navast/MapsActivity.java
@@ -1,19 +1,60 @@
 package com.example.icol.navast;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    double lat,longs;
+    LatLng current;
+    int complete;
+    LinearLayout topbar;
+    RelativeLayout appbar;
+    RelativeLayout middle_bar;
+    ImageButton pick;
+    final LatLng GOJEK = new LatLng(-6.27314, 106.81657);
+    private void inisialisasi() {
 
+        appbar = (RelativeLayout) findViewById(R.id.app_bar);
+        topbar = (LinearLayout) findViewById(R.id.middle_bar);
+        pick = (ImageButton) findViewById(R.id.pick);
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +78,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        inisialisasi();
+        complete=0;
+        //buildGoogleApiClient();
+        //mGoogleApiClient.connect();
+       // inf.setVisibility(View.GONE);
+       // strt.setVisibility(View.GONE);
+       // text_tujuan.setVisibility(View.GONE);
+       // but_tujuan.setVisibility(View.GONE);
+        CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(GOJEK, 13);
+        mMap.moveCamera(cameraPosition);
+        mMap.animateCamera(cameraPosition);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void pick_button(View view){
+        topbar.setVisibility(View.GONE);
+        pick.setVisibility(View.GONE);
+        Toast.makeText(this,"Sentuh Peta",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat=location.getLatitude();
+        longs=location.getLongitude();
+        current =new LatLng(lat,longs);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- AQMNS/NavAst/app/src/main/java/com/example/icol/navast/SplashScreen.java
+++ AQMNS/NavAst/app/src/main/java/com/example/icol/navast/SplashScreen.java
@@ -1,18 +1,33 @@
 package com.example.icol.navast;
 
-import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
- * Created by icol on 25/03/2017.
+ * Created by icol on 08/02/2017.
  */
-
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MapsActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
