dc006c83ac0bf7d9c24c43f6c716cc1145753a73
faisholl <faisho.el.ha@gmail.com>
2017-03-25 15:39:06 +0700

    INITIAL_COMMIT

---
+++ AQMNS/NavAst/app/src/main/java/com/example/icol/navast/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.icol.navast;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by icol on 25/03/2017.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+    }
+}
